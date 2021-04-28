98a2676056bcd7b525584ef43fe728481633f82c
soonitoon <soonitoon@gmail.com>
2021-04-28 17:18:54 +0900

    Add description

--- linear_regrassion.py
+++ linear_regrassion.py
@@ -11,13 +11,16 @@ X = [
     [186]
 ]
 y = [71,55,46,38,88]
-reg.fit(X, y)
+reg.fit(X, y) # 학습
 
 print(reg.predict([[165]]))
 
+# 학습 데이터와 y 값을 산포도로 그리기
 plt.scatter(X, y, color='black')
 
+# 학습 데이터를 입력으로 해서 예측값 계산
 y_pred = reg.predict(X)
 
+# 회귀선 그리기
 plt.plot(X, y_pred, color='blue', linewidth=3)
 plt.show()
