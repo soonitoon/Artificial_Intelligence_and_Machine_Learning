b3872609b9de22128d1ac0f37d4d41579333cedd
soonitoon <soonitoon@gmail.com>
2021-04-29 16:11:29 +0900

    Style: Change code format
    Because of Auto Code Formatter

--- linear_regrassion.py
+++ linear_regrassion.py
@@ -10,14 +10,17 @@ X = [
     [128],
     [186]
 ]
-y = [71,55,46,38,88]
-reg.fit(X, y)
+y = [71, 55, 46, 38, 88]
+reg.fit(X, y)  # 학습
 
 print(reg.predict([[165]]))
 
+# 학습 데이터와 y 값을 산포도로 그리기
 plt.scatter(X, y, color='black')
 
+# 학습 데이터를 입력으로 해서 예측값 계산
 y_pred = reg.predict(X)
 
+# 회귀선 그리기
 plt.plot(X, y_pred, color='blue', linewidth=3)
 plt.show()
