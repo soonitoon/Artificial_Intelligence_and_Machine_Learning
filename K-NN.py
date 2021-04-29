14bcbc41e10e52877959f4d90d898a2150e85ade
soonitoon <soonitoon@gmail.com>
2021-04-29 17:24:06 +0900

    Add data check code

--- K-NN.py
+++ K-NN.py
@@ -5,3 +5,14 @@ import numpy as np
 # 시각화 패키지
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# 샘플 데이터 로드
+iris = load_iris()
+
+# key value 확인
+print(iris)
+print(iris.DESCR)
+
+# label
+print(iris.data)
+print(iris.feature_names)
