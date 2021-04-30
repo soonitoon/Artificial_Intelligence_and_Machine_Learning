f5263bdf138a31e8050923b503eb10128efa65b2
soonitoon <soonitoon@gmail.com>
2021-04-30 11:10:19 +0900

    Set iris data

--- Iris-K-mean-clustering.py
+++ Iris-K-mean-clustering.py
@@ -2,3 +2,9 @@ from sklearn import datasets
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.cluster import KMeans
+
+# 데이터 로드
+iris = datasets.load_iris()
+
+X = iris.data[:, :2]
+y = iris.target
