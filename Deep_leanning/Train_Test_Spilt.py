da4a8dc2ff98983a54a094b4a26c80b9a8fdda52
soonitoon <soonitoon@gmail.com>
2021-05-06 20:40:48 +0900

    Add Data Split code

--- Deep_leanning/Train_Test_Spilt.py
+++ Deep_leanning/Train_Test_Spilt.py
@@ -10,3 +10,9 @@ y = iris.iloc[:,4].values
 encoder = LabelEncoder()
 y1 = encoder.fit_transform(y)
 Y = pd.get_dummies(y1).values
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
