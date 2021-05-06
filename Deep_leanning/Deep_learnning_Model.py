8b8c44ec4434a143b3e5cdfa6a23667dec8b8d69
soonitoon <soonitoon@gmail.com>
2021-05-06 20:59:37 +0900

    Import dependency modules

--- Deep_leanning/Deep_learnning_Model.py
+++ Deep_leanning/Deep_learnning_Model.py
@@ -1,6 +1,10 @@
 import seaborn as sns
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
 
 iris = sns.load_dataset("iris")
 
