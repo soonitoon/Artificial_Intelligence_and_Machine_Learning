e94ff0751b97bd120099c142f8bd6adb54098193
soonitoon <soonitoon@gmail.com>
2021-05-06 19:21:48 +0900

    Chore: Add description

--- Deep_leanning/Iris_Visualization.py
+++ Deep_leanning/Iris_Visualization.py
@@ -3,5 +3,5 @@ import pandas as pd
 import numpy as np
 
 sns.set(style="ticks", color_codes=True)
-iris = sns.load_dataset("iris")
+iris = sns.load_dataset("iris") # sameple data
 g = sns.pairplot(iris, hue="species", palette="husl")
