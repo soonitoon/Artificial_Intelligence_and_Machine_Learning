477d8d24762cb405a89f2f0811b841d6d921417b
soonitoon <soonitoon@gmail.com>
2021-05-06 21:00:29 +0900

    Add learnning model

--- Deep_leanning/Deep_learnning_Model.py
+++ Deep_leanning/Deep_learnning_Model.py
@@ -18,3 +18,13 @@ Y = pd.get_dummies(y1).values
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+
+model = Sequential()
+
+model.add(Dense(64, input_shape=(4,), activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(3, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+
+hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100)
