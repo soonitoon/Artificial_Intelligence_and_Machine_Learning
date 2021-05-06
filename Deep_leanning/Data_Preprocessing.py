a6c0226f8aad318f4bc93227e8013adae9bf7f1c
soonitoon <soonitoon@gmail.com>
2021-05-06 19:31:48 +0900

    Add data preprocessing

---
+++ Deep_leanning/Data_Preprocessing.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+iris = sns.load_dataset("iris")
+
+X = iris.iloc[:,0:4].values
+y = iris.iloc[:,4].values
+
+encoder = LabelEncoder()
+y1 = encoder.fit_transform(y)
+Y = pd.get_dummies(y1).values
+
+print(Y)
